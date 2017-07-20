265b19916774db5a5e5919bade8ba2ca8d4eaf9b
Bitterholz <simon.wienecke94@googlemail.com>
2017-07-20 18:43:51 +0200

    - Cleanup imports - Added some Docu - Added state information to NetworkNode(isDestination and number of connections) - fixed fuk in bounding boxes

--- src/main/java/com/sots/routing/NetworkNode.java
+++ src/main/java/com/sots/routing/NetworkNode.java
@@ -15,16 +15,23 @@ public class NetworkNode {
 	public int t_cost = 1;
 	private UUID id;
 	private IRoutable pipe;
+	private boolean isDestination;
 	
 	public NetworkNode(UUID id, IRoutable pipe) {
 		this.id=id;
 		this.pipe=pipe;
+		this.isDestination=false;
 	}
 
 	public void addNeighbor(NetworkNode node, int index) {
 		neighbors[index]=node;
 	}
 	
+	/**
+	 * Returns a specific neighbor of this node, specified by the index.
+	 * @param index Integer parameter representing the EnumFacing of the neighbor. Order is D-U-N-S-W-E (0-5)
+	 * @return Specific Node neighbor for the given side.
+	 */
 	public NetworkNode getNeighborAt(int index) {
 		return neighbors[index];
 	}
@@ -45,6 +52,30 @@ public class NetworkNode {
 	
 	public IRoutable getMember() {return pipe;}
 	
+	/**
+	 * Returns the Integer count of connections this pipe holds
+	 * @return Integer between 0 and 6
+	 */
+	public int connections() {
+		int out = 0;
+		for(NetworkNode item : neighbors) {
+			if(item!=null) {
+				out++;
+			}
+		}
+		return out;
+	}
+	/**
+	 * Wether or not this Node is a Destination
+	 */
+	public boolean isDestination() {return isDestination;}
+	
+	public void setAsDestination(boolean isDestination) {this.isDestination = isDestination;}
+	
+	/**
+	 * Get the Manhattan distance to the Target
+	 * @param target The Target Node
+	 */
 	public void h_Cost(NetworkNode target) {
 		h_cost = ((Math.abs(target.pipe.posX() - pipe.posX())+(Math.abs(target.pipe.posY() - pipe.posY())+(Math.abs(target.pipe.posZ() - pipe.posZ())))));
 	}
